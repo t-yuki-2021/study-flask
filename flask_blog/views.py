06b876e8588c2c8898a25125280c3a53c8daf364
Yuki Tanio <yuki_tanio@yahoo.co.jp>
2021-03-14 16:29:09 +0900

    session実装

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -5,6 +5,8 @@ from flask_blog import app
 
 @app.route('/')
 def show_entries():
+    if not session.get('logged_in'):
+        return redirect('/login')
     return render_template('entries/index.html')
 
 
@@ -16,10 +18,12 @@ def login():
         elif request.form['password'] != app.config['PASSWORD']:
             print('パスワードが異なります')
         else:
+            session['logged_in'] = True
             return redirect('/')
     return render_template('login.html')
 
 
 @app.route('/logout')
 def logout():
+    session.pop('logged_in', None)
     return redirect('/')
